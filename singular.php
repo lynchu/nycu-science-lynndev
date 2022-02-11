c50895dd1a94ab200472e65bb4ecc53ada21c1f3
tychuang <tychuang.cs10@nycu.edu.tw>
2022-02-11 11:43:32 +0800

    Update post layout

--- singular.php
+++ singular.php
@@ -1,8 +1,8 @@
 <?php 
 get_header(); 
 // Start the Loop
-while ( have_posts() ) : 
-	the_post();
+while ( have_posts() ) : the_post();
+	$post_id = get_the_ID();
 ?>
 
 <div id="post-title-area">
@@ -14,9 +14,44 @@ while ( have_posts() ) :
 	<div id="post-content"><?php the_content(); ?></div>
 	<?php if(is_single()) { get_template_part('include/template-parts/post', 'sidebar'); } ?>
 </div>
-
+<?php endwhile; ?>
+<!-- End of the loop. -->
+<?php 
+if ( get_post_type( $post_id ) == 'news' ) :
+	$args = array(  
+		'post__not_in' => array($post_id),
+		'post_type' => 'news',
+		'post_status' => 'publish',
+		'posts_per_page' => 4, 
+		'orderby' => 'date', 
+		'order' => 'DEC', 
+	);
+?>
+<div id="post-list-area">
+	<h2>下則公告</h2>
+	<div id="post-list-wrap">
+		<div id="arrow-previous" class="arrow">←</div>
+		<ul id="post-list">
+<?php 
+	$loop = new WP_Query( $args ); 
+	while ( $loop->have_posts() ) : $loop->the_post(); 
+?>		
+		<li class="post-list-item">
+			<dev class="round-edge"></dev>
+			<dev class="date"><?php echo get_the_date('Y.m.d'); ?></dev>
+			<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+		</li>
 <?php
-endwhile; 
-// End of the loop. 
+		//the_tags( 'Tags: ', ', ', '<br />' );
+	endwhile; 
+?>
+		</ul>
+		<div id="arrow-next" class="arrow">→</div>
+	</div>
+</div>
+
+	<!-- Do special_cat stuff... -->
+<?php 
+endif;
 get_footer();
 ?>
