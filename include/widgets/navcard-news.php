5a6b92460fac63ca39d792bb397e2d24a4ea2c41
tychuang <polarbear.cs05@g2.nctu.edu.tw>
2022-03-16 13:04:58 +0800

    Add sticky post feature

--- include/widgets/navcard-news.php
+++ include/widgets/navcard-news.php
@@ -1,47 +1,102 @@
 <div class="navcard navcard_l">
 <div class="navcard_left">
 <?php
-
-    $post_args = array(
-        'numberposts' => 5,
-        'post_type' => 'any',
+    
+    $counter = 0; // # of sticky posts
+    $posts_per_page = 5;
+    $sticky = get_option( 'sticky_posts' ); // Get all sticky posts
+    $args = array(  
         'category_name' => 'announcements,awards,sdgs',
+        'post_status' => 'publish',
+        'posts_per_page' => $posts_per_page, 
+        'orderby' => 'date', 
+        'order' => 'DEC',
+        'post__in' => $sticky
     );
+    /* 
+        Passing an empty array to post__in will return has_posts() as true (and all 
+        posts will be returned). Logic should be used before hand to determine if 
+        WP_Query should be used in the event that the array being passed to 
+        post__in is empty.
+     */
+    if( isset( $sticky[0] )):
 
-    $posts = get_posts($post_args);
+        $loop = new WP_Query( $args ); 
+        // Loop for sticky posts
+        if( $loop->have_posts() ):
 
-    if($posts):
-
-        foreach(get_posts($post_args) as $post):
-            $the_cat = get_the_category();
-            $category_name = $the_cat[0]->cat_name;
+            while ( $loop->have_posts() ) : $loop->the_post(); 
+                $post_id = get_the_ID();
+                $the_cat = get_the_category();
+                $category_name = $the_cat[0]->cat_name;
 ?>
-<div class="nav_card_item category_item" title="<?php echo $post->post_title; ?>">
-    <a href="<?php echo get_permalink($post); ?>">
+<div class="nav_card_item category_item" title="<?php the_title(); ?>">
+    <a href="<?php the_permalink(); ?>">
         <div class="date">
 <?php
-            if($title === 'events')
-                echo '演講日期 ｜' . get_field('event_begin_date', $post) . ' ~ ' . get_field('event_end_date', $post);
-            else
-                echo get_post_time('Y.m.d', false, $post);
+                if($title === 'events')
+                    echo '演講日期 ｜' . get_field('event_begin_date', $post_id) . ' ~ ' . get_field('event_end_date', $post_id);
+                else
+                    echo get_the_date('Y.m.d'); 
 ?>
         </div><br>
-        <div class="post_title ellipsis"><?php echo $post->post_title; ?></div><br><br><br>
+        <div class="post_title ellipsis"><?php the_title(); ?></div><br><br><br>
         <!-- <div class="category_name"><?php #echo $category_name ?></div> -->
     </a>
 </div>
 <?php
-        endforeach;
+            $counter++;
+            endwhile;
+        endif;
+        wp_reset_postdata();
+    endif;
+?>
+<?php
+    // Second loop for non-sticky posts
+    $args = array(  
+        'category_name' => 'announcements,awards,sdgs',
+        'post_status' => 'publish',
+        'posts_per_page' => $posts_per_page-$counter, 
+        'orderby' => 'date', 
+        'order' => 'DEC',
+        'post__not_in' => $sticky
+    );
+    $loop = new WP_Query( $args ); 
 
-    // there's no posts with this category
-    else:
+    if( $loop->have_posts() ):
+
+        while ( $loop->have_posts() ) : $loop->the_post(); 
+            $post_id = get_the_ID();
+            $the_cat = get_the_category();
+            $category_name = $the_cat[0]->cat_name;
+?>
+<div class="nav_card_item category_item" title="<?php the_title(); ?>">
+<a href="<?php the_permalink(); ?>">
+    <div class="date">
+<?php
+        if($title === 'events')
+            echo '演講日期 ｜' . get_field('event_begin_date', $post_id) . ' ~ ' . get_field('event_end_date', $post_id);
+        else
+            echo get_the_date('Y.m.d'); 
+?>
+    </div><br>
+    <div class="post_title ellipsis"><?php the_title(); ?></div><br><br><br>
+    <!-- <div class="category_name"><?php #echo $category_name ?></div> -->
+</a>
+</div>
+<?php
+        $counter++;
+        endwhile;
+    endif;
+    wp_reset_postdata();
+    
+    if ( $counter == 0 ):
 ?>
 <div class="nav_card_item">
     沒有這個分類的文章
 </div>
-<?php
+<?php 
     endif;
-    wp_reset_postdata();
 ?>
 </div>
 <div class="nav_card_more">
