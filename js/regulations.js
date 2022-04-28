38a7603eaa1b36ca25140202b15e826cb01b9f1d
Terry Chan <terrychan999@gmail.com>
2022-04-28 06:57:54 +0000

    fix path

--- js/regulations.js
+++ js/regulations.js
@@ -1,3 +1,3 @@
 $(() => {
-    $(".wp-block-button__link").prepend('<img id="post-sidebar-download-icon" src="https://dev2.terry.taipei/science/wp-content/themes/nycu-science/images/download-icon.png" alt="">');
+    $(".wp-block-button__link").prepend('<img id="post-sidebar-download-icon" src="../wp-content/themes/nycu-science/images/download-icon.png" alt="">');
 });
