ec720022ea11a2cbfeb57d302421e44094d828ac
a24203740 <johnny11118@gmail.com>
2022-08-12 06:57:54 +0800

    add page timelines complete page timelines as PC screen and laptop screen
    Co-authored-by: linoil <linoil@users.noreply.github.com>
    Co-authored-by: lynchu <lynchu@users.noreply.github.com>

---
+++ page-timelines.php
@@ -0,0 +1,420 @@
+<?php
+get_header();
+?>
+
+<div id="post-title-area">
+    <img id="title-img" src="<?php echo get_template_directory_uri()?>/images/timelines/title.png" alt="">
+    <h1 id="post-title">發展簡史</h1>
+</div>
+<div id="post-content">
+    <div id="vertical-dash">
+        
+    </div>
+    <table id="timelines-all">
+        <tbody>
+            <tr>
+                <th class="decades-indicator">1960s</th>
+                <td>
+                    <table class="timeline-subright">
+                        <tr>
+                            <th>1964</th>
+                        <td>電子物理系成立－隸屬電子研究所</td>
+                        </tr>
+                        <tr>
+                            <th>1967</th>
+                            <td>電子研究所改制為工學院：電子物理系隸屬工學院</td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+            <tr>
+                <th class="decades-indicator">1970s</th>
+                <td>
+                    <table class="timeline-subright">
+                        <tr>
+                            <th>1972</th>
+                            <td>應用數學系成立－隸屬工學院</td>
+                        </tr>
+                        <tr>
+                            <th>1977</th>
+                            <td>應用數學所成立碩士班－隸屬工學院</td>         
+                        </tr>
+                        <tr>
+                            <th>1979</th>
+                            <td>
+                                <ul>
+                                    <li>交通大學恢復為大學，理學院成立
+                                    <li>電子物理系轉入理學院－學士班學生仍授予工學士學位
+                                    <li>應用數學系轉入理學院（由博愛校區遷入光復校區管理一館）
+                                </ul>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>1980</th>
+                            <td>
+                                <ul>
+                                    <li>光電工程研究所成立－隸屬電子物理系
+                                    <li>資訊科學系成立
+                                </ul>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+            <tr>
+                <th class="decades-indicator">1980s</th>
+                <td>
+                    <table class="timeline-subright">
+                        <tr>
+                            <th>1982</th>
+                            <td>應用化學研究所成立（新生館）</td>
+                        </tr>
+                        <tr>
+                            <th>1983</th>
+                            <td>
+                                <ul>
+                                    <li>科學一館落成啟用
+                                    <li>電子物理系（含光電工程研究所）遷入科學一館
+                                </ul>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>1984</th>
+                            <td>應用數學系所合一，遷入科學一館，成立應用數學博士班</td>
+                        </tr>
+                        <tr>
+                            <th>1988</th>
+                            <td>光電工程研究所為獨立所</td>
+                        </tr>
+                        <tr>
+                            <th>1990</th>
+                            <td>應用化學系成立</td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+            <tr>
+                <th class="decades-indicator">1990s</th>
+                <td>
+                    <table class="timeline-subright">
+                        <tr>
+                            <th>1992</th>
+                            <td>
+                                <ul>
+                                    <li>統計學研究所成立（科學一館）
+                                    <li>應用化學系成立博士班
+                                    <li>科學二館落成啟用；理學院、應用化學系所遷入科學二館
+                                </ul>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>1993</th>
+                            <td>物理研究所成立碩士班</td>
+                        </tr>
+                        <tr>
+                            <th>1994</th>
+                            <td>生物科技研究所成立</td>
+                        </tr>
+                        <tr>
+                            <th>1995</th>
+                            <td>
+                                <ul>
+                                    <li>光電工程研究所轉歸電機資訊學院
+                                    <li>資訊科學系轉歸電機資訊學院
+                                </ul>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>1996</th>
+                            <td>
+                                <ul>
+                                    <li>統計學研究所遷入綜合一館
+                                    <li>物理研究所自博愛校區遷入光復校區計網中心三樓
+                                </ul>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>1997</th>
+                            <td>統計學研究所成立博士班</td>
+                        </tr>
+                        <tr>
+                            <th>1998</th>
+                            <td>生物科技研究所成立博士班</td>
+                        </tr>
+                        <tr>
+                            <th>1999</th>
+                            <td>
+                                <ul>
+                                    <li>物理研究所成立博士班
+                                    <li>生物科技系成立
+                                </ul>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+            <tr>
+                <th class="decades-indicator">2000s</th>
+                <td>
+                    <table class="timeline-subright">
+                        <tr>
+                            <th>2001</th>
+                            <td>
+                                <ul>
+                                    <li>理學院碩士在職專班成立
+                                    <li>生化工程研究所成立
+                                </ul>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>2002</th>
+                            <td>生物資訊研究所成立</td>
+                        </tr>
+                        <tr>
+                            <th>2004</th>
+                            <td>
+                                <ul>
+                                    <li>分子科學研究所成立
+                                    <li>生物科技學院成立;生物科技系所、生化工程研究所、生物資訊研究所轉歸生物科技學院
+                                    <li>電子物理系遷入工程六館
+                                    <li>理學院設置傑出研究院長獎
+                                </ul>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>2006</th>
+                            <td>理學院科學學士學位學程班（簡稱科學學士班）成立</td>
+                        </tr>
+                        <tr>
+                            <th>2007</th>
+                            <td>
+                                <ul>
+                                    <li>數學建模與科學計算碩士班成立
+                                    <li>理學院傑出研究院長獎更名理學院年輕學者研究獎
+                                </ul>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>2009</th>
+                            <td>
+                                <ul>
+                                    <li>科學計算與數學建模中心成立
+                                    <li>田家炳光電中心大樓落成啟用
+                                </ul>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+            <tr>
+                <th class="decades-indicator">2010s</th>
+                <td>
+                    <table class="timeline-subright">
+                        <tr>
+                            <th>2011</th>
+                            <td>基礎科學教學研究大樓（簡稱科學三館）落成啟用</td>
+                        </tr>
+                        <tr>
+                            <th>2012</th>
+                            <td>
+                                <ul>
+                                    <li>丘成桐中心成立（科學一館）
+                                    <li>理學院、電子物理系、物理研究所、碩士在職專班、科學學士班遷入科學三館
+                                </ul>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>2016</th>
+                            <td>科學計算與數學建模中心完成階段性任務，中心業務結束</td>
+                        </tr>
+                        <tr>
+                            <th>2017</th>
+                            <td>理學院年輕學者研究獎更名理學院創新教學獎及年輕學者研究獎</td>
+                        </tr>
+                        <tr>
+                            <th>2018</th>
+                            <td>分子科學研究中心完成階段性任務，中心業務結束</td>
+                        </tr>
+                        <tr>
+                            <th>2019</th>
+                            <td>基礎科學教學研究大樓更名科學三館</td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+            <tr>
+                <th class="decades-indicator">2020s</th>
+                <td>
+                    <table class="timeline-subright">
+                        <tr>
+                            <th>2021</th>
+                            <td>理論與計算物理中心成立（科學三館）</td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+<!-- End of the loop. -->
+<style>
+    .decades-indicator
+    {
+        font-size: 4rem;
+        font-weight: bold;
+        padding: 0;
+        vertical-align: top;
+    }
+    .decades-indicator::after
+    {
+        position: absolute; /* 定位點是 #timelines-all>tbody>tr */
+        top: 40px;
+        left: calc(180px + 9%);
+        width: 40px;
+        height: 40px;
+        background-color: #1A30FF;
+        border-radius: 50%;
+        content:"";
+    }
+    #timelines-all > tbody > tr:last-child > .decades-indicator::after
+    {
+        color: #1A30FF;
+        top: 30px;
+        left: calc(170px + 9%);
+        width: 60px;
+        height: 60px;
+        background-color: #F6F3EA;
+        border: 2px solid gray;
+        font-size: 50px;
+        line-height: 52px;
+        text-indent: 3.5px;
+        text-align: center;
+        content:"．";
+    }
+    #timelines-all
+    {
+        margin-top: 15vh;
+        width:100%;
+        border-collapse: separate;
+    }
+    #timelines-all>tbody>tr
+    {
+        position: relative;
+    }
+    #timelines-all>tbody>tr>td
+    {
+        padding-top: 30px;
+        padding-left: 23%;
+    }
+    #timelines-all>tbody>tr>th, #timelines-all>tbody>tr>td
+    {
+        border-bottom: 100px solid transparent;
+    }
+    #timelines-all > tbody > tr:last-child > th, #timelines-all > tbody > tr:last-child > td
+    {
+        border-bottom: 0;
+    }
+    #post-content
+    {
+        position: relative;
+    }
+    #vertical-dash
+    {
+        position: absolute; /* 定位點是 #post-content */
+        top: 40px;
+        left: calc(200px + 9%);/* 200px 是 decade indicator 的大小 */
+        width: 0;
+        border-right: 3.2px dashed gray;
+        opacity: 25%;
+        background: none;
+        height: calc(100% - 80px);
+    }
+    .timeline-subright
+    {
+        font-size: 1.25rem;
+        text-align: left;
+        width: 100%;
+    }
+    .timeline-subright>tbody>tr>td
+    {
+        padding-right: 0;
+        padding-left: 5%;
+    }
+    .timeline-subright>tbody>tr>th
+    {
+        width: 5vw;
+        letter-spacing: 1px;
+        vertical-align:top;
+    }
+    .timeline-subright ul
+    {
+        padding-left:0;
+        padding-top:0;
+        padding-bottom:0;
+        margin-top:0;
+        
+    }
+    .timeline-subright ul li
+    {
+        list-style: none;
+    }
+    .timeline-subright ul li::before
+    {
+        content:"·";
+        font-size:1.25rem;
+    }
+    #title-img
+    {
+        max-width: 25%;
+        height: auto;
+        margin-right: 15%;
+    }
+    #post-title
+    {
+        margin-top: 10%;
+        letter-spacing: 3px;
+    }
+    table tr td, table tr th
+    {
+        border: 0;
+    }
+    @media only screen and (max-width: 1439px) and (min-width: 992px) {
+        #vertical-dash
+        {
+            left: calc(175px + 9%);/* 175px 是 decade indicator 的大小 */
+        }
+        .decades-indicator::after
+        {
+            top: 35px;
+            left: calc(155px + 9%);
+        }
+        #timelines-all > tbody > tr:last-child > .decades-indicator::after
+        {
+            top: 25px;
+            left: calc(145px + 9%);
+        }
+    }
+    @media only screen and (max-width: 991px) {
+        #vertical-dash 
+        {
+            left: calc(150px + 9%);/* 150px 是 decade indicator 的大小 */
+        }
+        .decades-indicator::after
+        {
+            top: 25px;
+            left: calc(130px + 9%);
+        }
+        #timelines-all > tbody > tr:last-child > .decades-indicator::after
+        {
+            top: 15px;
+            left: calc(120px + 9%);
+        }
+    }
+    
+</style>
+
+
+<?php 
+get_footer();
+?>
+
