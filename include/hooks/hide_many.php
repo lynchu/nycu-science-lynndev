8721c45ac056f8b3cb296870a7325daa5c4a5cef
Terry Chan <terrychan999@gmail.com>
2022-08-19 13:52:20 +0800

    update remove shortlink syntax

--- include/hooks/hide_many.php
+++ include/hooks/hide_many.php
@@ -5,6 +5,14 @@ remove_action('wp_head', 'wlwmanifest_link');
 remove_action('wp_head', 'rest_output_link_wp_head', 10);
 remove_action('template_redirect', 'rest_output_link_header', 11, 0);
 remove_action('wp_head', 'wp_generator');
+
+
+// remove HTML meta tag
+// <link rel='shortlink' href='http://example.com/?p=25' />
+remove_action('wp_head', 'wp_shortlink_wp_head', 10);
+
+// remove HTTP header
+// Link: <https://example.com/?p=25>; rel=shortlink
 remove_action('template_redirect', 'wp_shortlink_header', 11);
 
 // hide rss generator version
