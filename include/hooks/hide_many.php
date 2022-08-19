a500ff647afc5746bbf857ad8326547cbc2dcff1
Terry Chan <terrychan999@gmail.com>
2022-08-19 13:21:21 +0800

    remove html header shortlink

--- include/hooks/hide_many.php
+++ include/hooks/hide_many.php
@@ -5,6 +5,7 @@ remove_action('wp_head', 'wlwmanifest_link');
 remove_action('wp_head', 'rest_output_link_wp_head', 10);
 remove_action('template_redirect', 'rest_output_link_header', 11, 0);
 remove_action('wp_head', 'wp_generator');
+remove_action('template_redirect', 'wp_shortlink_header', 11);
 
 // hide rss generator version
 function remove_wp_version_rss() {
